ad413c82c1e00167c0bf8dc2620caf8965c9e678
mechlin <mechlin@hotmail.com>
2025-03-03 13:22:00 -0800

    Add files via upload

---
+++ EMsoftOO_compile.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+apt-get update -y
+apt-get upgrade -y
+apt-get install build-essential git gcc gfortran cmake ocl-icd-opencl-dev libblas-dev liblapack-dev -y
+
+cd /home/mechlin/Build
+
+#EMsoft Superbuild
+git clone https://github.com/EMsoft-org/EMsoftSuperbuild.git -b developOO EMsoftSuperbuildOO
+cd EMsoftSuperbuildOO
+# mkdir Debug
+# cd Debug
+# cmake -DEMsoftOO_SDK=/home/mechlin/Build/EMsoftSuperbuildOO -DCMAKE_BUILD_TYPE=Debug ../
+# make -j
+# cd ../
+mkdir Release
+cd Release
+cmake -DEMsoftOO_SDK=/home/mechlin/Build/EMsoftSuperbuildOO/ -DCMAKE_BUILD_TYPE=Release ../
+make -j
+
+cd /home/mechlin/Build 
+ 
+#EMsoftOO Build
+git clone https://github.com/EMsoft-org/EMsoftOO.git -b develop EMsoftOO
+mkdir EMsoftOOBuild
+cd EMsoftOOBuild
+mkdir Release
+cd Release
+cmake -DCMAKE_BUILD_TYPE=Release -DEMsoftOO_SDK=/home/mechlin/Build/EMsoftSuperbuildOO/ ../../EMsoftOO
+make
+#cd ../
+#mkdir Debug
+#cd Debug
+#cmake -DCMAKE_BUILD_TYPE=Debug -DEMsoftOO_SDK=/home/mechlin/Build/EMsoftSuperbuildOO/ ../../EMsoftOO
+#make -j
+ 
